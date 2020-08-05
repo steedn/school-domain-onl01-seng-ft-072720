2af49cee5a3d90fe9c1eef8fb87518151a6429a0
Nathan Steed <steedn@outlook.com>
2020-08-05 23:16:18 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,7 +1,8 @@
 # code here!
 class School
-  attr_accessor :name, :grade
+  attr_accessor :roster, :add_student
   attr_reader :school
+  NEW = []
   def initialize(school)
     @school = school
   end
